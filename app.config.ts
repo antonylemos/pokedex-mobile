d658e487dd91a8726a6ac7b13424937f4a1fe5ea
antony <djalma.rodrigues@luizalabs.com>
2024-08-18 11:00:21 -0300

    build(project): setup typescript

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+import 'ts-node/register';
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: "pokedex-app",
+  slug: "pokedex-app",
+  version: "1.0.0",
+};
+
+export default config;
